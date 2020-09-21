565af7b6da492d92582d6664935e6be16b088f30
dede20011982 <dede20011982@gmail.com>
2020-09-20 20:49:13 -0500

    "created hero component"

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from './Button';
+import './HeroSection.css';
+import '../App.css';
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <img src="public\assets\flagofusa.gif" alt=""/>
+            <h1>Harris County Deputies Relief Fund Inc.</h1>
+            <p>"We take care of our own"</p>
+            <div className="hero-btns">
+                <Button
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                >
+                    INDIVIDUAL SUPPORTERS
+                </Button>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--primary'
+                    buttonSize='btn--large'
+                >
+                    BUSINESS SUPPORTERS
+                </Button>
+            </div>
+        </div>
+    );    
+}
+
+export default HeroSection;
